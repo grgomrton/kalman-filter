a5148db2b31afeb25b2c606e38793de507b4b9fa
marc <grgo.mrton@gmail.com>
2018-03-18 00:47:21 +0100

    hello snowhouse
    added snowhouse source
    custom translator for assertionexception

--- tests/FactorialTests.cpp
+++ tests/FactorialTests.cpp
@@ -1,12 +1,19 @@
 #include "catch2/catch.hpp"
+#include "snowhouse/snowhouse.h"
+
+using namespace snowhouse;
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("Factorials are computed") {
+    AssertThat(Factorial(1), Is().EqualTo(1));
+    AssertThat(Factorial(2), Is().EqualTo(2));
+    AssertThat(Factorial(3), Is().EqualTo(6));
+    AssertThat(Factorial(10), Is().EqualTo(3628800));
+}
+
+TEST_CASE("A failing test case should display an explanation for the failure") {
+    AssertThat(1, Is().EqualTo(2));
 }
--- tests/Init.cpp
+++ tests/Init.cpp
@@ -1,2 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+#include <snowhouse/assertionexception.h>
+
+CATCH_TRANSLATE_EXCEPTION(snowhouse::AssertionException& ex) {
+    return ex.GetMessage();
+}

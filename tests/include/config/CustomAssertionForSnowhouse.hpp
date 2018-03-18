9d6a99633212cace0b83f87c1ad2b6f2bf3f9c22
marc <grgo.mrton@gmail.com>
2018-03-18 01:35:45 +0100

    header guard

--- tests/include/config/CustomAssertionForSnowhouse.hpp
+++ tests/include/config/CustomAssertionForSnowhouse.hpp
@@ -1,6 +1,11 @@
+#ifndef KALMAN_FILTER_CUSTOMASSERTIONFORSNOWHOUSE_HPP
+#define KALMAN_FILTER_CUSTOMASSERTIONFORSNOWHOUSE_HPP
+
 #include "catch2/catch.hpp"
 #include <snowhouse/assertionexception.h>
 
 CATCH_TRANSLATE_EXCEPTION(snowhouse::AssertionException& ex) {
     return ex.GetMessage();
 }
+
+#endif //KALMAN_FILTER_CUSTOMASSERTIONFORSNOWHOUSE_HPP

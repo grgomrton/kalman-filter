0dcca112a27415e2437f01ede96927a831c049f8
marc <grgo.mrton@gmail.com>
2018-03-25 16:48:16 +0200

    hello fakeit

--- tests/TestEnvironmentTests.cpp
+++ tests/TestEnvironmentTests.cpp
@@ -1,6 +1,8 @@
 #include "catch2/catch.hpp"
+#include "fakeit/standalone/fakeit.hpp"
 #include "snowhouse/snowhouse.h"
 
+using namespace fakeit;
 using namespace snowhouse;
 
 TEST_CASE("A failing test case should display an explanation for the failure", "[ignore]") {
@@ -15,4 +17,21 @@ TEST_CASE("Snowhouse can evaluate a complex expression while evaluating collecti
 
     AssertThat(values, Has().Exactly(1).GreaterThan(2).And().LessThan(4));
     //AssertThat(values, Has().Exactly(1).GreaterThan(4).And().LessThan(5)); // should fail
+}
+
+struct SomeInterface {
+    virtual int foo(int) = 0;
+};
+
+TEST_CASE("FakeIt should be able to mock an interface and verify calls to it", "[ignore]") {
+    Mock<SomeInterface> mock;
+    When(Method(mock, foo)).AlwaysReturn(1);
+    int someIrrelevantParameter = 10;
+
+    SomeInterface& i = mock.get();
+    auto fooValue = i.foo(someIrrelevantParameter);
+
+    Verify(Method(mock, foo)).Exactly(1);
+    //Verify(Method(mock, foo)).Exactly(2); // should display reason
+    AssertThat(fooValue, Is().EqualTo(1));
 }

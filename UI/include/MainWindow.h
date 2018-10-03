cb5f05560cab233325baba1f1576f20e1cd85355
marc <grgo.mrton@gmail.com>
2018-10-03 17:48:45 +0200

    movement update on plot
    button to mainwindow
    update current position
    plotgaussian
    make mainwindow non-copyable

--- UI/include/MainWindow.h
+++ UI/include/MainWindow.h
@@ -3,13 +3,32 @@
 #include <gtkmm/window.h>
 #include <gtkmm/grid.h>
 #include <gtkmm-plplot/canvas.h>
+#include <gtkmm/button.h>
+#include <Localizer.h>
+#include <gtkmm-plplot/plotdata2d.h>
+#include <gtkmm-plplot.h>
 
 class MainWindow : public Gtk::Window {
 public:
     MainWindow();
+    MainWindow(const MainWindow& other) = delete;
+    MainWindow& operator=(const MainWindow& other) = delete;
+    ~MainWindow() override;
 
 private:
+    GaussianDistributionDescriptor currentPosition;
+    Localizer localizer;
+
+    std::vector<double> x_scale;
     Gtk::Grid layout;
     Gtk::PLplot::Canvas canvas;
-    static std::vector<double> uniformScale(double start, double end, unsigned int reference_point_count);
+    Gtk::PLplot::PlotData2D* plotData;
+    Gtk::PLplot::Plot2D plot;
+    Gtk::Button moveLeftButton;
+
+    void moveLeftClicked();
+    void invalidate();
+
+    static std::vector<double> uniformScale(double start, double end, unsigned int reference_point_count); // plotter?
+    static std::vector<double> plotGaussian(double mean, double variance, const std::vector<double>& scale);
 };
--- UI/src/MainWindow.cpp
+++ UI/src/MainWindow.cpp
@@ -2,38 +2,36 @@
 #include <gtkmm/label.h>
 #include <gtkmm-plplot/plot2d.h>
 #include <Localizer.h>
+#include <MainWindow.h>
 
-MainWindow::MainWindow() {
-    GaussianDistributionDescriptor initial_position_localizer = GaussianDistributionDescriptor::GaussianDistributionDescriptor(0.0, 0.1); // 0.0 +/-10 cm
-    Localizer localizer(initial_position_localizer);
+
+MainWindow::MainWindow() : // todo x_scale here!
+    currentPosition(GaussianDistributionDescriptor::makeFromAccuracy(0.0, 0.1)), // 0.0 +/-10 cm
+    localizer(currentPosition),
+    plotData(nullptr),
+    plot("x", ""), // todo extract to consts
+    moveLeftButton("Move left") {
 
     auto label = Gtk::manage(new Gtk::Label("Current estimation: "));
     label->set_margin_bottom(10);
     layout.attach_next_to(*label, Gtk::POS_BOTTOM, 1, 1);
 
-    GaussianDistributionDescriptor current_estimation = localizer.getPosition();
-    auto y_stdddev = [](double x, double mean, double variance) { return 1 / (sqrt(variance)*sqrt(2*M_PI))*pow(M_E, -0.5*pow((x-mean)/sqrt(variance), 2)); }; // ugh
-    std::string x_caption = "x";
-    std::string y_caption = "";
     unsigned int reference_point_count = 1000;
     double start = -10;
     double end = 10;
-    std::vector<double> x_values = uniformScale(start, end, reference_point_count);
-    std::vector<double> y_values;
-    for (double x : x_values) {
-        y_values.push_back(y_stdddev(x, current_estimation.getPosition(), current_estimation.getVariance()));
-    }
-
-    auto plot_data = Gtk::manage(new Gtk::PLplot::PlotData2D(x_values, y_values, Gdk::RGBA("red")));
-    auto plot = Gtk::manage(new Gtk::PLplot::Plot2D(*plot_data, x_caption, y_caption));
-    canvas.add_plot(*plot);
+    x_scale = uniformScale(start, end, reference_point_count);
+    canvas.add_plot(plot);
 
     canvas.set_hexpand(true);
     canvas.set_vexpand(true);
     layout.attach_next_to(canvas, Gtk::POS_BOTTOM, 1, 1);
 
+    moveLeftButton.signal_clicked().connect(sigc::mem_fun(*this, &MainWindow::moveLeftClicked));
+    layout.attach_next_to(moveLeftButton, Gtk::POS_BOTTOM, 1, 1);
+
     add(layout);
     set_border_width(10);
+    invalidate();
     show_all_children();
 }
 
@@ -44,5 +42,36 @@ std::vector<double> MainWindow::uniformScale(double start, double end, unsigned 
         scale[i] = start + i * step;
     }
     return scale;
+}
+
+void MainWindow::moveLeftClicked() {
+    currentPosition = localizer.movementUpdate(-1.0, 20);
+    invalidate();
+    std::cout << "current position mean: " << currentPosition.getPosition() << " variance: " << currentPosition.getVariance() << std::endl;
+    std::cout << "updated!" << std::endl;
+}
+
+std::vector<double> MainWindow::plotGaussian(double mean, double variance, const std::vector<double>& scale) {
+    auto fx = [](double x, double mean, double variance) { return 1 / (sqrt(variance)*sqrt(2*M_PI))*pow(M_E, -0.5*pow((x-mean)/sqrt(variance), 2)); }; // ugh
+    std::vector<double> y_values(scale.size());
+    for (int i=0; i<y_values.size(); i++) {
+        y_values[i] = fx(scale[i], mean, variance);
+    }
+    return y_values;
+}
+
+void MainWindow::invalidate() {
+    if (plotData != nullptr) {
+        plot.remove_data(*plotData);
+    }
+    delete plotData;
+    plotData = nullptr;
+    std::vector<double> y_values = plotGaussian(currentPosition.getPosition(), currentPosition.getVariance(), x_scale);
+    plotData = new Gtk::PLplot::PlotData2D(x_scale, y_values, Gdk::RGBA("red"));
+    plot.add_data(*plotData);
+}
+
+MainWindow::~MainWindow() {
+    delete plotData;
 }
 

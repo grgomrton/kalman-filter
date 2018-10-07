433b9bc67064b4380db0ce54ec7aff57d61855b8
marc <grgo.mrton@gmail.com>
2018-10-07 04:41:04 +0200

    remove nullptr equality check
    can safely done by indicating type in name

--- src/Plotter.cpp
+++ src/Plotter.cpp
@@ -4,33 +4,31 @@
 #include <gtkmm-plplot/plotdata2d.h>
 #include <PlotFunctions.h>
 
-Plotter::Plotter(std::shared_ptr<Gtk::PLplot::Plot2D> plot, std::vector<double> scale) {    // todo check pointers
-    mScale = std::move(scale);
+Plotter::Plotter(std::shared_ptr<Gtk::PLplot::Plot2D> plot, std::vector<double> scale) {
+    mScale = std::move(scale);      // todo check pointers
     mpPlot = std::move(plot);
 }
 
 void Plotter::AddBelief(GaussianDistributionDescriptor pose) {
-    std::vector<double> y_values = PlotFunctions::PlotGaussian(pose.getPosition(), pose.getVariance(), mScale);
+    auto y_values = PlotFunctions::PlotGaussian(pose.getPosition(), pose.getVariance(), mScale);
 
-    if (mpLastPosition != nullptr) { mpPlot->remove_data(*mpLastPosition); }
+    if (mpLastPosition) { mpPlot->remove_data(*mpLastPosition); }
     mpLastPosition = std::make_unique<Gtk::PLplot::PlotData2D>(mScale, y_values, Gdk::RGBA("red"));
     mpPlot->add_data(*mpLastPosition);
 
-    if (mpLastMeasurement != nullptr) { mpPlot->remove_data(*mpLastMeasurement); }
+    if (mpLastMeasurement) { mpPlot->remove_data(*mpLastMeasurement); }
     mpLastMeasurement.reset();
 }
 
 void Plotter::AddMeasurement(GaussianDistributionDescriptor measurement) {
-    std::vector<double> y_values = PlotFunctions::PlotGaussian(measurement.getPosition(),
-                                                               measurement.getVariance(),
-                                                               mScale);
+    auto y_values = PlotFunctions::PlotGaussian(measurement.getPosition(), measurement.getVariance(), mScale);
 
-    if (mpLastMeasurement != nullptr) { mpPlot->remove_data(*mpLastMeasurement); }
+    if (mpLastMeasurement) { mpPlot->remove_data(*mpLastMeasurement); }
     mpLastMeasurement = std::make_unique<Gtk::PLplot::PlotData2D>(mScale, y_values, Gdk::RGBA("blue"));
     mpPlot->add_data(*mpLastMeasurement);
 }
 
 Plotter::~Plotter() {
-    if (mpLastPosition != nullptr) { mpPlot->remove_data(*mpLastPosition); }
-    if (mpLastMeasurement != nullptr) { mpPlot->remove_data(*mpLastMeasurement); }
+    if (mpLastPosition) { mpPlot->remove_data(*mpLastPosition); }
+    if (mpLastMeasurement) { mpPlot->remove_data(*mpLastMeasurement); }
 }
